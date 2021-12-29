2fbe85342055b5b6a0200bd0ddc69995efc7a0b2
Charlie Wang <charlie@charliewang.io>
2021-12-29 10:44:34 -0500

    Add check constraint for react count
    This should be in a new migration but no one is using this so it's OK.

--- migrations/001-initial.sql
+++ migrations/001-initial.sql
@@ -1,6 +1,6 @@
 CREATE TABLE config (
     guild_id TEXT PRIMARY KEY,
     archive_channel_id TEXT NOT NULL,
-    react_count INTEGER NOT NULL DEFAULT 9,
+    react_count INTEGER NOT NULL DEFAULT 9 CHECK (react_count >= 1),
     react_trigger TEXT NOT NULL DEFAULT '📌'
 );
